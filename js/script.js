c8735d0b7d32b5b4b32a5d2231fb8588eec2c681
Akiiiiiii51 <buffengine20720@gmail.com>
2022-03-29 09:38:56 +0900

    second commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+//マウスオーバー時の処理
+function mouseOn(){
+  var obj = document.getElementById("img1");
+  obj.src = "./images/unu3.jpg";
+}
+
+//マウスアウト時の処理
+function mouseOff(){
+  var obj = document.getElementById("img1");
+  obj.src = "./images/うぬぼれラジオ_トリッキー3.jpg";
+}ß
---
+++ js/script2.js
@@ -0,0 +1,12 @@
+
+
+function mouseOn(){
+  var obj = document.getElementById("img2");
+  obj.src = "./images/tato2.jpg";
+}
+
+//マウスアウト時の処理
+function mouseOff(){
+  var obj = document.getElementById("img2");
+  obj.src = "./images/guitar.jpg";
+}
